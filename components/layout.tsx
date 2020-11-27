bca1347e54e50184ae71f1052158a2fccc30ff94
Georg Ludewig <fischermatte@posteo.net>
2020-11-27 14:15:02 +0100

    ongoing nextjs experimenting

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 type LayoutProps = {
   children: any
@@ -15,7 +16,149 @@ export default class Layout extends React.Component<LayoutProps> {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <title>Georg Ludewig, Software Engineer in Bernlocal</title>
         </Head>
-        <header className="w-full p-6 bg-gray-300">Header</header>
+        <header className="w-full p-6 bg-gray-300">
+          {/*<nav className="bg-gray-800">*/}
+          {/*  <Link href="/">*/}
+          {/*    <a>Home</a>*/}
+          {/*  </Link>*/}
+          {/*  <Link href="/resume">*/}
+          {/*    <a>Resume</a>*/}
+          {/*  </Link>*/}
+          {/*  <Link href="/projects">*/}
+          {/*    <a>Projects</a>*/}
+          {/*  </Link>*/}
+          {/*  <Link href="/contact">*/}
+          {/*    <a>Contact</a>*/}
+          {/*  </Link>*/}
+          {/*</nav>*/}
+          <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+              <div className="relative flex items-center justify-between h-16">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  <button
+                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    aria-expanded="false"
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    {/*<svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"*/}
+                    {/*     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">*/}
+                    {/*    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"*/}
+                    {/*          d="M4 6h16M4 12h16M4 18h16"/>*/}
+                    {/*</svg>*/}
+                    {/*<svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"*/}
+                    {/*     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">*/}
+                    {/*    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"*/}
+                    {/*          d="M6 18L18 6M6 6l12 12"/>*/}
+                    {/*</svg>*/}
+                  </button>
+                </div>
+                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex-shrink-0 flex items-center">
+                    {/*<img className="block lg:hidden h-8 w-auto"*/}
+                    {/*     src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">*/}
+                    {/*<img className="hidden lg:block h-8 w-auto"*/}
+                    {/*     src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"*/}
+                    {/*     alt="Workflow">*/}
+                  </div>
+                  <div className="hidden sm:block sm:ml-6">
+                    <div className="flex space-x-4">
+                      <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900">
+                        Dashboard
+                      </a>
+                      <a
+                        href="#"
+                        className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+                      >
+                        Team
+                      </a>
+                      <a
+                        href="#"
+                        className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+                      >
+                        Projects
+                      </a>
+                      <a
+                        href="#"
+                        className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+                      >
+                        Calendar
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="sr-only">View notifications</span>
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                    </svg>
+                  </button>
+
+                  <div className="ml-3 relative">
+                    <div>
+                      {/*<button*/}
+                      {/*    className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"*/}
+                      {/*    id="user-menu" aria-haspopup="true">*/}
+                      {/*    <span className="sr-only">Open user menu</span>*/}
+                      {/*    <img className="h-8 w-8 rounded-full"*/}
+                      {/*         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"*/}
+                      {/*         alt="">*/}
+                      {/*</button>*/}
+                    </div>
+                    <div
+                      className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="user-menu"
+                    >
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+                        Your Profile
+                      </a>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+                        Settings
+                      </a>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+                        Sign out
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="hidden sm:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900">
+                  Dashboard
+                </a>
+                <a
+                  href="#"
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+                >
+                  Team
+                </a>
+                <a
+                  href="#"
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+                >
+                  Projects
+                </a>
+                <a
+                  href="#"
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700"
+                >
+                  Calendar
+                </a>
+              </div>
+            </div>
+          </nav>
+        </header>
         <main className="w-full  p-6 bg-gray-100">{children}</main>
         <footer className="w-full p-6 bg-gray-300"> Footer</footer>
       </div>

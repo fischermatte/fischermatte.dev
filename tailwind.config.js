c50404969c3aebcea35b33cbb00f70df6573b01b
Georg Ludewig <fischermatte@posteo.net>
2020-12-01 01:02:29 +0100

    ongoing flexing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 const {colors} = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
       ...colors,
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
     },
     extend: {},
   },

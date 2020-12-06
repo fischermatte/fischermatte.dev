7d61f64f272ca921220c897852e64f6aaed659ad
fischermatte <fischermatte@posteo.net>
2020-12-06 17:50:56 +0100

    added under construction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import LayoutComponent from '../components/layout.component'
 import * as React from 'react'
+import {faWrench} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 interface Props {
   title?: string
@@ -9,7 +11,10 @@ export default class Home extends React.Component<Props> {
   render(): JSX.Element {
     return (
       <LayoutComponent>
-        <h1>Under Construction</h1>
+        <div className="text-7xl w-full text-center text-accent-normal py-12 bg-accent-normal text-accent-dark">
+          <div className="text-2xl mb-10 font-bold">Under Construction</div>
+          <FontAwesomeIcon icon={faWrench} />
+        </div>
       </LayoutComponent>
     )
   }

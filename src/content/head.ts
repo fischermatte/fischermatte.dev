0eb8d2ad95697f4b5edd89cf456b9fb5c44db5d8
fischermatte <fischermatte@users.noreply.github.com>
2020-12-16 12:52:51 +0100

    update head

--- src/content/head.ts
+++ src/content/head.ts
@@ -3,21 +3,20 @@ export const head = {
     title: 'Fischermatte | Cloud Software Engineer',
     description: 'I am a Software Engineer located in Bern, Switzerland.',
     keywords:
-      'fischermatte, engineering, cloud, serverless, domain driven design, tailwindcss, typescript, software engineer, software development, bern, switzerland, schweiz',
+      'fischermatte',
   },
   resume: {
     title: 'Resume | Fischermatte | Cloud Software Engineer',
     description: 'Everything about my education, certificates and work experience',
-    keywords: 'resume, cv, education, certificates, university, master, workplace, location',
-  },
+    keywords: 'resume,
   projects: {
     title: 'Projects | Fischermatte | Cloud Software Engineer',
     description: 'The projects I was involved in since 2002',
-    keywords: 'projects, portfolio, cloud, aws, firebase, serverless, nosql',
+    keywords: 'projects',
   },
   contact: {
     title: 'Contact | Fischermatte | Cloud Software Engineer',
     description: "Any questions or need help? Don't hesitate to contact me.",
-    keywords: 'contact, fischermatte, email, message',
+    keywords: 'contact',
   },
 }

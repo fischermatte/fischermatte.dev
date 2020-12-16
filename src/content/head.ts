95c9b98f44d118995eb91050945c77b1063aa8c5
fischermatte <georg.ludewig@mobiliar.ch>
2020-12-16 13:05:53 +0100

    fixed content

--- src/content/head.ts
+++ src/content/head.ts
@@ -2,21 +2,21 @@ export const head = {
   index: {
     title: 'Fischermatte | Cloud Software Engineer',
     description: 'I am a Software Engineer located in Bern, Switzerland.',
-    keywords:
-      'fischermatte',
+    keywords: 'fischermatte',
   },
   resume: {
     title: 'Resume | Fischermatte | Cloud Software Engineer',
     description: 'Everything about my education, certificates and work experience',
-    keywords: 'resume,
-  projects: {
-    title: 'Projects | Fischermatte | Cloud Software Engineer',
-    description: 'The projects I was involved in since 2002',
-    keywords: 'projects',
-  },
-  contact: {
-    title: 'Contact | Fischermatte | Cloud Software Engineer',
-    description: "Any questions or need help? Don't hesitate to contact me.",
-    keywords: 'contact',
+    keywords: 'resume',
+    projects: {
+      title: 'Projects | Fischermatte | Cloud Software Engineer',
+      description: 'The projects I was involved in since 2002',
+      keywords: 'projects',
+    },
+    contact: {
+      title: 'Contact | Fischermatte | Cloud Software Engineer',
+      description: "Any questions or need help? Don't hesitate to contact me.",
+      keywords: 'contact',
+    },
   },
 }

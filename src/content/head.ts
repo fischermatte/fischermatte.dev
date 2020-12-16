ad4876eb212ae9a627511d0380a1bfc02adf6134
fischermatte <georg.ludewig@mobiliar.ch>
2020-12-16 13:07:02 +0100

    fixed content

--- src/content/head.ts
+++ src/content/head.ts
@@ -8,15 +8,15 @@ export const head = {
     title: 'Resume | Fischermatte | Cloud Software Engineer',
     description: 'Everything about my education, certificates and work experience',
     keywords: 'Resume',
-    projects: {
-      title: 'Projects | Fischermatte | Cloud Software Engineer',
-      description: 'The projects I was involved in since 2002',
-      keywords: 'Projects',
-    },
-    contact: {
-      title: 'Contact | Fischermatte | Cloud Software Engineer',
-      description: "Any questions or need help? Don't hesitate to contact me.",
-      keywords: 'Contact',
-    },
+  },
+  projects: {
+    title: 'Projects | Fischermatte | Cloud Software Engineer',
+    description: 'The projects I was involved in since 2002',
+    keywords: 'Projects',
+  },
+  contact: {
+    title: 'Contact | Fischermatte | Cloud Software Engineer',
+    description: "Any questions or need help? Don't hesitate to contact me.",
+    keywords: 'Contact',
   },
 }

c4d392eaae77ae8b63691008411ccfaa047259bd
fischermatte <fischermatte@posteo.net>
2020-12-18 00:38:31 +0100

    fixed update query

--- src/pages/api/phrases/[id]/like.ts
+++ src/pages/api/phrases/[id]/like.ts
@@ -1,14 +1,23 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {Phrase} from '../../../../shared/phrase.types'
 import {ErrorResponseBody} from '../../../../shared/common.types'
+import {query as q} from 'faunadb'
+import {fauna} from '../../../../utils/fauna-client'
 
 export default async function like(
   req: NextApiRequest,
   res: NextApiResponse<Pick<Phrase, 'totalLikes'> | ErrorResponseBody>,
 ): Promise<void> {
-  const totalLikes: number = parseInt(req.body.totalLikes)
+  const phraseId = req.query.id
   try {
-    res.status(200).json({totalLikes: totalLikes + 1})
+    const result: {data: Phrase} = await fauna.query(
+      q.Update(q.Ref(q.Collection('phrases'), phraseId), {
+        data: {
+          totalLikes: q.Add(1, q.Select(['data', 'totalLikes'], q.Get(q.Ref(q.Collection('phrases'), phraseId)))),
+        },
+      }),
+    )
+    res.status(200).json({totalLikes: result.data.totalLikes})
   } catch (e) {
     // something went wrong
     res.status(500).json({error: e.message})

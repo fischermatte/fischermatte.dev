381583afe3312bb45e0ddd49c59887018a7e7864
Georg Ludewig <georg.ludewig@mobi.ch>
2021-06-30 10:18:50 +0200

    chore: removed husky dependency, introduced eslint-json

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   },
   ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: ['next'],
+  extends: ['next', 'plugin:json/recommended'],
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],

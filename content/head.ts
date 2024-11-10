b03cb3980bfeba192c4d6c510625b2800e85a1ae
G. Ludewig <fischermatte@users.noreply.github.com>
2024-11-10 18:00:52 +0100

    Update head.ts

--- content/head.ts
+++ content/head.ts
@@ -1,9 +1,9 @@
 export const head = {
   index: {
-    title: 'Fischermatte | Cloud Software Engineer',
+    title: 'Klaus Native | Cloud Software Engineer',
     description:
       'I am a Software Engineer located in Bern, Switzerland. Currently focusing on Serverless and Cloud native web development.',
-    keywords: 'Fischermatte',
+    keywords: 'Klaus Native',
   },
   resume: {
     title: 'Resume | Fischermatte | Cloud Software Engineer',
@@ -11,12 +11,12 @@ export const head = {
     keywords: 'Resume',
   },
   projects: {
-    title: 'Projects | Fischermatte | Cloud Software Engineer',
+    title: 'Projects | Klaus Native | Cloud Software Engineer',
     description: 'The projects I was involved in since 2002',
     keywords: 'Projects',
   },
   contact: {
-    title: 'Contact | Fischermatte | Cloud Software Engineer',
+    title: 'Contact | Klaus Native | Cloud Software Engineer',
     description: "Any questions or need help? Don't hesitate to contact me.",
     keywords: 'Contact',
   },
